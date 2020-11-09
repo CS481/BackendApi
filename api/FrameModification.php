427db5e86eb4e5fa8dd5d05d28e0bdb5eb211720
Cayden Reynolds <creynolds1@ycp.edu>
2020-11-09 18:24:03 -0500

    Switched some stuff to controllers in other repo

--- api/FrameModification.php
+++ api/FrameModification.php
@@ -1,18 +1,14 @@
 <?php
 require_once('SimulationFactoryBackend/src/db/DBConnFactory.php');
 require_once('SimulationFactoryBackend/src/util/check_method.php');
+require_once('SimulationFactoryBackend/src/controller/FrameController.php');
 SimulationFactoryBackend\util\only_allow_method('POST');
 $data = json_decode(file_get_contents('php://input'), false);
 $db_conn_class = SimulationFactoryBackend\db\DBConnFactory();
 $conn = $db_conn_class::constructFromJson($data);
 try {
   $conn->beginTransaction();
-  $search_for->username = $data->user->username;
-  $search_for->_id = $data->frame_id;
-  $data->username = $data->user->username;
-  unset($data->user);
-  unset($data->frame_id);
-  $results = $conn->update('Frames', $data, $search_for);
+  SimulationFactoryBackend\controller\modify_frame($conn, $data->user, $data, $data->frame_id);
   $conn->submitTransaction();
 } catch (Exception $e)  {
   $conn->abortTransaction();
