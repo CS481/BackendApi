9bf4e3bfdb67bd9d0893c388410aa6d7a5d2f98d
Cayden Reynolds <creynolds1@ycp.edu>
2020-11-23 15:41:38 -0500

    Committing to change branch

--- api/SimulationModification.php
+++ api/SimulationModification.php
@@ -2,7 +2,7 @@
 require_once('SimulationFactoryBackend/src/db/DBConnFactory.php');
 require_once('SimulationFactoryBackend/src/util/check_method.php');
 require_once('SimulationFactoryBackend/src/controller/SimulationController.php');
-require_once('simulation-schema/php/IdRequest.php');
+require_once('simulation-schema/php/SimModValidator.php');
 
 SimulationFactoryBackend\util\only_allow_method('POST');
 $data = json_decode(file_get_contents('php://input'), false);
